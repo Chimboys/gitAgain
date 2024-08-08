76adb5a55ebd21bdf1df3bbc4045318a89de9fcc
Chimboys <akmal@usf.edu>
2024-08-08 19:25:58 -0400

    Showcased default dict

--- ex.py
+++ ex.py
@@ -2,6 +2,16 @@
 
 from collections import defaultdict, Counter
 
-fruits = ["apple", "banana", "apple"]
-appearance_of_fruits = Counter(fruits)
-print(appearance_of_fruits)
+list_example = ["apple", "banana", "apple"]
+appearance_of_elements = Counter(list_example)
+# Allows to count how many times each element appeared in a collection
+# Counter(collection)
+print(appearance_of_elements)
+
+dict_example = defaultdict(int)
+print(dict_example["A"])
+#By using default dict, we do not get error even if the key does not point to any value. Emulates default value
+dict_example["A"] = "First letter of alphabet"
+#Then, we can easily change its default value
+print(dict_example["A"])
+
