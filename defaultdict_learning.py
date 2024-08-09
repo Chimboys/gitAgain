7ab909ba75a7cb67d9fe895f130440b967707dd2
Chimboys <akmal@usf.edu>
2024-08-08 23:41:18 -0400

    Splitting ex.py to a seperate files for each data structure

---
+++ defaultdict_learning.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+''' Counter , instead of creating dict and the iterate list, just use COUNTER'''
+list_example = ["apple", "banana", "apple"]
+appearance_of_elements = Counter(list_example)
+# Allows to count how many times each element appeared in a collection
+# Counter(collection)
+print(appearance_of_elements)
+
+dict_example = defaultdict(int)
+print(dict_example["A"])
+#By using default dict, we do not get error even if the key does not point to any value. Emulates default value
+dict_example["A"] = "First letter of alphabet"
+#Then, we can easily change its default value
+print(dict_example["A"])
+
